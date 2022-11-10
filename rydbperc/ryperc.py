3f2df39a8e77efe48f7a180a7c9b3adb55e378d5
Valerio-Amico <valerio.amico98@gmail.com>
2022-11-10 19:19:43 +0100

    TO FINISH: evolve

--- rydbperc/ryperc.py
+++ rydbperc/ryperc.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
 from scipy.stats import truncnorm
+from rydbperc import utilities
 
 def truncated_normal(mean=0, sd=1, low=0, upp=1):
     """ 
@@ -38,6 +39,7 @@ class cluster3D:
         self.KDT = self.get_KDT()
         self.R_shell = None
         self.dR_shell = None
+        self.cluster_excited = []
         pass
 
     def get_KDT(self):
@@ -70,11 +72,52 @@ class cluster3D:
         ax.scatter(self.KDT.data[:,0],self.KDT.data[:,1],self.KDT.data[:,2], marker=".", alpha=1, s=100)
         plt.show()
 
-    def point_connections(self, point_index):
+    def get_points_connections(self, point_indeces):
         """ 
         retruns set of points (indeces) in the facilitation shell of the point with index "point_index".
         the facilitation shell is the shell with R in [R-dR/2, R+dR/2]. 
         """
-        big_ball = set(KDTree.query_ball_point(KDTree.data[point_index],self.R_shell+self.dR_shell/2))
-        small_ball = set(KDTree.query_ball_point(KDTree.data[point_index],self.R_shell-self.dR_shell/2))
+        big_ball = set(KDTree.query_ball_point(KDTree.data[point_indeces],self.R_shell+self.dR_shell/2))
+        small_ball = set(KDTree.query_ball_point(KDTree.data[point_indeces],self.R_shell-self.dR_shell/2))
         return big_ball.difference(small_ball)
+    
+    def set_evolution_parameters(self, shell_radius, shell_delta, p_spont_exct, p_fac, p_emission):
+        """ 
+        args:
+            shell_radius (float): radius of the facilitation shell
+            shell_delta (float): width of the facilitation shell
+                                 the facilitation shell is the shell with R in [R-dR/2, R+dR/2]
+            p_spont_exct (float in [0,1]): probability of spontaneous excitations in one step
+            p_fac (float in [0,1]): probability of facilitated excitation in one step, if 
+                                    facilitation condition is respected (i.e. the point is in the facilitiation shell of another point)
+            p_emission (float in [0,1]): probability of spontaneous emission in one step
+        """
+        self.R_shell = shell_radius
+        self.dR_shell = shell_delta
+        self.p_spont_exct = p_spont_exct
+        self.p_fac = p_fac
+        self.p_emission = p_emission
+
+    def evolve(self, steps):
+        """ 
+        evolves the system
+        args:
+            steps (int): evolution steps
+        """
+        for step in range(steps):
+            ####### spontaneus excitation ###################
+            N_spontaneous_exct = np.random.poisson(self.p_emission*self.size)
+            self.cluster_excited = list(set(self.cluster_excited + list(np.random.choice(np.range(self.size), N_spontaneous_exct))))
+            ####### end spontaneous excitation ##############
+            ####### facilitation excitation #################
+            facilitable_points = self.get_points_connections(self.cluster_excited)
+            N_fac_exct = np.random.binomial(len(facilitable_points),self.p_fac)
+            self.cluster_excited = list(set(self.cluster_excited + list(np.random.choice(facilitable_points, N_fac_exct))))
+            ####### end facilitation excitation #############
+            ####### spontaneous emission ####################
+            N_spontaneous_emission = np.random.binomial(len(self.cluster_excited), self.p_emission)
+             list(np.random.choice(np.range(self.size), N_spontaneous_emission))
+            self.cluster_excited = list(set(self.cluster_excited + )))
+            
+        return 
+
