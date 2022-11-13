df48280e72cebc951622ed3f432b8fbcd5f7a257
Valerio-Amico <valerio.amico98@gmail.com>
2022-11-13 16:36:47 +0100

    minors

--- 3D_percolation.py
+++ 3D_percolation.py
@@ -11,20 +11,20 @@ shape = [80,80,100]
 density = N_points/(shape[0]*shape[1]*shape[2]*10**-2) # atoms/cm^3
 
 C6 = 1.2*10**6 # van der waals coefficient [MHz * um^6]
-gamma_def = 4 # dephasing rate [MHz]
+gamma_def = 4.4 # dephasing rate [MHz]
 Delta = 20 # detuning [MHz]
 gamma_spontaneous_emission_70s = 0.1 # [MHz]
 
 radius_shell = (C6/(Delta))**(1/6)
 delta_radius_shell = radius_shell*(gamma_def/(6*Delta))
 
-dt = 1 # [us]
+dt = 0.5 # [us]
 evo_time = 100 # [us]
 N_steps = int(evo_time/dt)
 
 p_spont_exc = 2/(N_points) * dt/evo_time
 p_spont_emi = gamma_spontaneous_emission_70s * dt
-p_facilitation_range = np.linspace(0, 1, 40)
+p_facilitation_range = np.linspace(0, 0.1, 20)
 
 print("density = ",density)
 print("fac. shell radius and delta = ", radius_shell, delta_radius_shell)
@@ -37,7 +37,7 @@ N_exct_stds = []
 for i, p_facilitation in enumerate(p_facilitation_range):
     reps = []
     for _ in range(just_repetitions):
-        clu = ryperc.cluster3D(N_points, shape, distribution="gaussian", MOT_radius=radius_MOT)
+        clu = ryperc.cluster3D(N_points, shape, distribution="uniform")#, MOT_radius=radius_MOT)
 
         clu.set_evolution_parameters(
                                     shell_radius=radius_shell, 
